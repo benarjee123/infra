5d60ab80086d33e08b0a644a38ba238fe82291a7
benarjee123 <benarjee465@gmail.com>
2026-02-01 01:43:27 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "admin_username" {
+  type        = string
+  description = "Admin username for VM"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Admin password for VM"
+}
